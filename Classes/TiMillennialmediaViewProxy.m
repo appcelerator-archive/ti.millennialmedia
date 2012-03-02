ed8784dcf5dd36fbbabbc4f008e311f5fbab6477
Dawson Toth <dtoth@appcelerator.com>
2012-03-02 11:13:37 -0600

    [MOD-518] Adjusted to Use New Method

--- Classes/TiMillennialmediaViewProxy.m
+++ Classes/TiMillennialmediaViewProxy.m
@@ -15,13 +15,10 @@
 }
 
 #ifndef USE_VIEW_FOR_UI_METHOD
-	#define USE_VIEW_FOR_UI_METHOD(methodname)	\
-	-(void)methodname:(id)args	\
-	{	\
-		if ([self viewAttached])	\
-		{	\
-			[[self view] performSelectorOnMainThread:@selector(methodname:) withObject:args waitUntilDone:NO];	\
-		}	\
+	#define USE_VIEW_FOR_UI_METHOD(methodname)\
+	-(void)methodname:(id)args\
+	{\
+		[self makeViewPerformSelector:@selector(methodname:) withObject:args createIfNeeded:YES waitUntilDone:NO];\
 	}
 #endif
 USE_VIEW_FOR_UI_METHOD(refresh);
